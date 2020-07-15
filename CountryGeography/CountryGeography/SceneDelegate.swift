a749b643f792dd80e661960a1040c89c13995870
Dona Thomas <donathomas389@gmail.com>
2020-07-15 22:15:57 +0530

    Removed main storyboard for programatic addition of UI components

--- CountryGeography/CountryGeography/SceneDelegate.swift
+++ CountryGeography/CountryGeography/SceneDelegate.swift
@@ -17,7 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+                guard let windowScene = (scene as? UIWindowScene) else { return }
+
+                // Create the root view controller as needed
+                let dashboardViewController = CountryDetailsViewController()
+                let navigationController = UINavigationController(rootViewController: dashboardViewController)
+                let appWindow = UIWindow(windowScene: windowScene)
+                appWindow.rootViewController = navigationController
+                appWindow.makeKeyAndVisible()
+                window = appWindow
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
