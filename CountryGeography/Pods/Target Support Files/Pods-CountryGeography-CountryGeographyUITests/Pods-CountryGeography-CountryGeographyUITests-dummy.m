193b019abd9bf5f60ee2e88ad16b1f77dff77410
Dona Thomas <donathomas389@gmail.com>
2020-07-15 12:22:09 +0530

    Initial project setup with podfile

---
+++ CountryGeography/Pods/Target Support Files/Pods-CountryGeography-CountryGeographyUITests/Pods-CountryGeography-CountryGeographyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CountryGeography_CountryGeographyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CountryGeography_CountryGeographyUITests
+@end
